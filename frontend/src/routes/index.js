cd33b1ad32fef9aa6b4ce173b18de350875d96c5
Nguyen Duc Thuan <thuan452004@gmail.com>
2024-08-09 20:59:22 +0700

    Base Admin

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,33 +1,35 @@
-
 import LayoutDefault from "../layout/LayoutDefault";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Info from "../pages/Info";
 import InfoUser from "../pages/InfoUser";
-
+import AppAdmin from "../admin/Layout";
 
 export const routes = [
-    {
+  {
+    path: "/",
+    element: <LayoutDefault />,
+    children: [
+      {
         path: "/",
-        element: <LayoutDefault />,
-        children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-                path: "about",
-                element: <About />
-            },
-            {
-                path: "info",
-                element: <Info />
-            },
-            {
-                path: "infoUser",
-                element: <InfoUser />
-            }
-        ]
-    }
-]
-
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "info",
+        element: <Info />,
+      },
+      {
+        path: "infoUser",
+        element: <InfoUser />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AppAdmin />,
+  },
+];
