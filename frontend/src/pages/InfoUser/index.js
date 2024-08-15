caf0497c8e8e3d2cdf87fff8b4cc2f6a01181fea
Nam Nguyễn <hoangnam2584bn@gmail.com>
2024-08-15 21:37:50 +0700

    Update fill data my mentor

--- frontend/src/pages/InfoUser/index.js
+++ frontend/src/pages/InfoUser/index.js
@@ -121,29 +121,54 @@ const InfoUser = () => {
                 <Title level={4}>My Mentor</Title>
                 <div className="mentor-info-border"></div>
                 {myMentors.map((mentor) => (
-                  <Col key={mentor.id} span={12}>
-                    <Card
-                      className="mentor-info-subCard"
-                      title={mentor.name}
-                      extra={<img src={mentor.companyLogo} alt="Company Logo" />}
-                      cover={<img alt="avatar" src={mentor.avatar || defaultAvatar} />}
-                      actions={[
-                        <Button onClick={() => navigate(`/mentors/${mentor.slug}`)}>
-                          View more
-                        </Button>,
-                        <Button onClick={() => navigate(`/mentors/${mentor.slug}/review`)}>
-                          My Review
-                        </Button>,
-                      ]}
-                    >
-                      <p>Mentee Count: {mentor.menteeCount}</p>
-                      <p>Introduction 1: {mentor.introduction1}</p>
-                      <p>Introduction 2: {mentor.introduction2}</p>
-                      <p>
-                        Rate: {mentor.rate}/5 ({mentor.numberRate} ratings)
-                      </p>
-                    </Card>
-                  </Col>
+                  // <Col key={mentor.id} span={12}>
+                  //   <Card
+                  //     className="mentor-info-subCard"
+                  //     title={mentor.name}
+                  //     extra={<img src={mentor.companyLogo} alt="Company Logo" />}
+                  //     cover={<img alt="avatar" src={mentor.avatar || defaultAvatar} />}
+                  //     actions={[
+                  //       <Button onClick={() => navigate(`/mentors/${mentor.slug}`)}>
+                  //         View more
+                  //       </Button>,
+                  //       <Button onClick={() => navigate(`/mentors/${mentor.slug}/review`)}>
+                  //         My Review
+                  //       </Button>,
+                  //     ]}
+                  //   >
+                  //     <p>Mentee Count: {mentor.menteeCount}</p>
+                  //     <p>Introduction 1: {mentor.introduction1}</p>
+                  //     <p>Introduction 2: {mentor.introduction2}</p>
+                  //     <p>
+                  //       Rate: {mentor.rate}/5 ({mentor.numberRate} ratings)
+                  //     </p>
+                  //   </Card>
+                  // </Col>
+                  <Row>
+                    <Row key={mentor._id} className="mentorCard">
+                    <Col span={10} className='mentorCard-image'>
+                      <img
+                        src={mentor.avatar}
+                        alt="Avatar"
+                        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                      />
+                    </Col>
+                    <Col span={13} className='mentorCard-content'>
+                      <h3>{mentor.name}</h3>
+                      <div className='mentorCard-content-logo'>
+                        <img src={mentor.companyLogo} alt='Company logo' style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                      </div>
+                      <p className='mentorCard-content-menteeCount'>Mentee: {mentor.menteeCount}</p>
+                      <p className='mentorCard-content-introduction'>Mục giới thiệu 1: {mentor.introduction1}</p>
+                      <p className='mentorCard-content-introduction'>Mục giới thiệu 2: {mentor.introduction2}</p>
+                      <Rate className='mentorCard-content-rate' disabled defaultValue={mentor.rate} />
+                      <p className='mentorCard-content-rateCount'>({mentor.numberRate} đánh giá) ({mentor.rate}/5)</p>
+                      <Button className='mentorCard-content-Btn' onClick={handleApplyModal}>Apply now</Button>
+                      <ApplyModal open={isApplyModalVisible} onCancel={handleCancel} />
+                      <Button className='mentorCard-content-Btn' onClick={() => handleViewMore(mentor)}>View more</Button>
+                    </Col>
+                  </Row>
+                  </Row>
                 ))}
               </Row>
               <Row gutter={[16, 16]} className="mentor-info-item">
