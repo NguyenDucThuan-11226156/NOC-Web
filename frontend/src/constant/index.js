e712c133e44f2a4da5b6c0d6c0d26a49a50528df
NguyenDucThuan-11226156 <128773377+NguyenDucThuan-11226156@users.noreply.github.com>
2024-08-27 14:52:52 +0700

    Deploy

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -1,3 +1,3 @@
 export const limit = 8;
-export const API = "https://noc-pmtwil45k-thuans-projects.vercel.app";
+export const API = "https://noc-bxinvbh4p-thuans-projects.vercel.app/";
 // export const API = "nodm.com";
